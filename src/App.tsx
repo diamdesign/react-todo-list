4864508e58a49cb966fefe56826dd0070bccf400
Fredrik Berglund <fredrik@diam.se>
2024-02-28 09:24:04 +0100

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,56 @@
+import React, { useState } from "react";
+import { Todo } from "./components/Todo";
+
 export function App() {
-  return (
-    <>
-      <h1>This is the App component</h1>
-    </>
-  );
+	const [todos, setTodos] = useState<{ id: number; text: string }[]>([]);
+	const [todoInput, setTodoInput] = useState("");
+
+	const addTodo = (text: string) => {
+		if (text === "") {
+			return;
+		}
+		const newTodo = { id: Date.now(), text };
+		setTodos([...todos, newTodo]);
+		setTodoInput("");
+	};
+
+	const deleteTodo = (id: number) => {
+		setTodos(todos.filter((todo) => todo.id !== id));
+	};
+
+	const updateTodo = (id: number, newText: string) => {
+		setTodos(todos.map((todo) => (todo.id === id ? { ...todo, text: newText } : todo)));
+	};
+
+	const handleKeyUp = (e: React.KeyboardEvent<HTMLInputElement>) => {
+		if (e.key === "Enter") {
+			addTodo(todoInput.trim());
+		}
+	};
+
+	return (
+		<>
+			<div id="todos">
+				<h1>Todos</h1>
+				<input
+					id="inputtodo"
+					type="text"
+					onChange={(e) => setTodoInput(e.target.value)}
+					onKeyUp={handleKeyUp}
+					value={todoInput}
+				/>
+				<div id="list">
+					{todos.map((todo) => (
+						<Todo
+							key={todo.id}
+							id={todo.id}
+							text={todo.text}
+							onDelete={deleteTodo}
+							onUpdate={updateTodo}
+						/>
+					))}
+				</div>
+			</div>
+		</>
+	);
 }
